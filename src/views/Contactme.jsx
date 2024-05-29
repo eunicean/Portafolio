a917cf60f0034ebd8226d9080678119829c68e2a
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-05-29 04:47:38 -0600

    created an apart for contactme

---
+++ src/views/Contactme.jsx
@@ -0,0 +1,24 @@
+function Contactme() {
+    return (
+        <div className="contactinfo">
+            <div className="contactintro">
+                <p className="Tittle1"> Contact info</p>
+                <p>You can contact me if you need me</p>
+            </div>
+            <div className="contactlinks">
+                <div className="emailscontact">
+                    <p>eunice.mata2992@gmail.com</p>
+                    <p>mat21231@uvg.edu.gt</p>
+                </div>
+                <div className="media">
+                    <a href="https://github.com/eunicean"><img src="https://static-00.iconduck.com/assets.00/github-fill-icon-2048x1998-6810ky6f.png" alt="" /></a>
+                    <a href="https://www.linkedin.com/in/eunice-mata-15a814204/"><img src="https://cdn-icons-png.flaticon.com/512/61/61109.png" alt="" /></a>
+                    <a href="https://www.instagram.com/eu_mat_nel/"><img src="https://freelogopng.com/images/all_img/1683192079instagram-logo-black-and-white-png.png" alt="" /></a>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Contactme
