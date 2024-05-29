2f1641024d0566573421f8a42b8ca4b67b56ca07
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-05-29 04:47:20 -0600

    changed greetings

--- src/views/Greetings.jsx
+++ src/views/Greetings.jsx
@@ -1,8 +1,14 @@
 function Gretings(){
     return(
         <div className="greetings">
-            <h1> Eunice Mata</h1>
-            <h3> Portfolio</h3>
+            <div className="greetings-content">
+                <a href="https://github.com/eunicean"><img src="https://i.pinimg.com/736x/b5/1b/78/b51b78ecc9e5711274931774e433b5e6.jpg" alt="" /></a>
+                <p className="Tittle1">Software Developer</p>
+                <h1> Eunice Mata</h1>
+                <h3> Portfolio</h3>
+                <p>eunice.mata2992@gmail.com</p>
+
+            </div>
         </div>
     )
 }
