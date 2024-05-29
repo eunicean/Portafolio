1a6a924d15d16c5a9b19d8ae0246eced7061622f
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-05-29 04:46:53 -0600

    changed about me

--- src/views/Aboutme.jsx
+++ src/views/Aboutme.jsx
@@ -4,7 +4,8 @@ function About(){
         <div className="aboutmeSection">
             <div className="info">
                 <p className="Tittle2"> About me</p>
-                <p> A little about meee</p>
+                <p className="aboutmecontent"> I'm a software developer in the making. I consider myself a person that can do anything I propose myself to. I'm creative, intelligent and a people person. I'm alway motivated to try new things and learn.</p>
+                <p className="aboutmecontent"> I have expierence in software and web development, both in back and front development. I have obtained varios skills that i can use with confidence.</p>
             </div>
             <div> <img src={me} alt="Me" /></div>
         </div>
