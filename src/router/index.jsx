577cc3c91c3b1073038b34acf7998172c802d932
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-05-29 01:33:11 -0600

    router changed

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import useNavigate from "@hooks/useNavigate";
 
 import Menu from "@views/Menu"
+import Header from "@components/Header";
 
 const routes = {
     '/': {
@@ -14,15 +15,22 @@ const routes = {
 function Router() {
     const { page } = useNavigate();
 
-    const CurrentPage = routes[page]?.component || (() => (
+    let CurrentPage;
+
+    if (routes[page] && routes[page].component) {
+    CurrentPage = routes[page].component;
+    } else {
+    CurrentPage = () => (
         <div className='notFoundContainer'>
-            <h1 className='h1Glow'>404 Page Not Found</h1>
-            <h3> Please enter a valid URL</h3>
+        <h1 className='h1Glow'>Oops! Page Not Found</h1>
+        <h3>Please enter a valid URL</h3>
         </div>
-    ));
+    );
+    }
 
     return (
-        <div>            
+        <div> 
+            <Header/>           
             <CurrentPage />
         </div>
     );
