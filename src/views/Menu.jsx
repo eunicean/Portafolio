7ab77b9ca83f44f31f421f20b27b13ac76e5c3f1
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-05-29 06:47:18 -0600

    menu updated

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -3,15 +3,24 @@ import About from "./Aboutme";
 import Gretings from "./Greetings";
 import Skillset from "./Skillset";
 import Contactme from "./Contactme";
-
+import Projects from "./Projects";
+import achievement1 from "@assets/achievement1.jpg"
 
 function Menu() {
+
+  
+
     return(
         <>
             <Gretings />
             <About />
             <h3>Skills learned</h3>
             <Skillset />
+            <h3>Projects</h3>
+            <Projects />
+            <h3>Achievements</h3>
+            <img src={achievement1} alt="Graduado de bachiller" className="achievements"/>
+            <br></br>
             <Contactme />
         </>
     )
